679b3d5f41a4fca1be9eb264c442fe4fd7e8861b
Vishnyam <114306355+Vishnyam@users.noreply.github.com>
2022-10-14 22:34:51 +0300

    Update Practice4_7_33.c

--- Practice4/Project4/Practice4_7_33.c
+++ Practice4/Project4/Practice4_7_33.c
@@ -6,16 +6,16 @@ int main(int argc, char **argv){
 	FILE *f1, *f2;
 	f1 = fopen(argv[1], "r");
 	if(f1 == NULL) return 0;
-	f((str1 = get_str(f)) == NULL){
-		printf("%s\n", "Empty file");
-		fclose(f1);
-		return 0;
-	}
 	fseek (f, 0, SEEK_SET);
 	f2 = fopen("file123.txt", "w+");
 	if(f2 == NULL) return 0;
 	int len = atoi(argv[2]);
 	char *str = malloc(sizeof(char) * (len + 1));
+        if((str = get_str(f)) == NULL){
+		printf("%s\n", "Empty file");
+		fclose(f1);
+		return 0;
+	}
 	while(fgets(str, len, f1))
 		fputs(str, f2);
 	fclose(f1);
