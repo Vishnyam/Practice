1848d33899ae099bdc30c0a37d409f88cd1718eb
Vishnyam <114306355+Vishnyam@users.noreply.github.com>
2022-10-14 22:37:58 +0300

    Update Practice4_7_33.c
    Edit

--- Practice4/Project4/Practice4_7_33.c
+++ Practice4/Project4/Practice4_7_33.c
@@ -6,7 +6,6 @@ int main(int argc, char **argv){
 	FILE *f1, *f2;
 	f1 = fopen(argv[1], "r");
 	if(f1 == NULL) return 0;
-	fseek (f, 0, SEEK_SET);
 	f2 = fopen("file123.txt", "w+");
 	if(f2 == NULL) return 0;
 	int len = atoi(argv[2]);
@@ -16,6 +15,7 @@ int main(int argc, char **argv){
 		fclose(f1);
 		return 0;
 	}
+        fseek (f, 0, SEEK_SET);
 	while(fgets(str, len, f1))
 		fputs(str, f2);
 	fclose(f1);
